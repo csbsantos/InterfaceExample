47f75d337e7a142612b247347f5068b3951f787a
csbsantos <claudiasbsantos13@gmail.com>
2025-09-30 16:54:01 +0100

    Update Circle.java

--- src/Circle.java
+++ src/Circle.java
@@ -1,4 +1,20 @@
-package PACKAGE_NAME;
+public class Circle extends GeometricFigure{
 
-public class Circle {
+    public Circle(int area) {
+        super(area);
+    }
+
+    @Override
+    public void draw(int area) {
+        System.out.println("You just draw a circle with " + area + " cm2");
+
+    }
+
+    @Override
+    public void fill(int area) {
+        System.out.println("You just filled  a circle with " + area + " cm2");
+
+    }
+
+
 }
