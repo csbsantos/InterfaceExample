b03d6f26de1114823223dd4f40bf4f4f02944814
csbsantos <claudiasbsantos13@gmail.com>
2025-09-30 16:55:08 +0100

    Update GeometricFigure.java

--- src/GeometricFigure.java
+++ src/GeometricFigure.java
@@ -1,4 +1,10 @@
-package PACKAGE_NAME;
+public abstract class GeometricFigure implements Drawable, Fillable{
+    private int area;
 
-public class GeometricFigure {
+    public GeometricFigure(int area) {
+        this.area = area;
+    }
+
+    public abstract void draw (int area);
+    public abstract void fill (int area);
 }
