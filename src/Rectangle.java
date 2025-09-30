c0557304b4389085e14d6c08cd94e3d9f59ca5ab
csbsantos <claudiasbsantos13@gmail.com>
2025-09-30 16:55:47 +0100

    Update Rectangle.java

--- src/Rectangle.java
+++ src/Rectangle.java
@@ -1,4 +1,14 @@
-package PACKAGE_NAME;
+public class Rectangle extends GeometricFigure{
+    public Rectangle(int area) {
+        super(area);
+    }
 
-public class Rectangle {
+    @Override
+    public void draw (int area){
+        System.out.println("You just draw a rectangle with" + area + " cm2");
+    }
+    @Override
+    public void fill (int area){
+        System.out.println("You just filled a rectangle with" + area + " cm2");
+    }
 }
