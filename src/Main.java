2a6f0970e6ece08cfd42377c8329f800eb6a5a8f
csbsantos <claudiasbsantos13@gmail.com>
2025-09-30 16:55:28 +0100

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        Circle circle;
+        Rectangle rectangle;
+        GeometricFigure geometricFigure;
+        Stickman stickman;
+        Circle circle1 = new Circle(12);
+        Rectangle rectangle1 = new Rectangle(20);
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            System.out.println("i = " + i);
-        }
-    }
-}
+
+        Stickman stickman1= new Stickman();
+        circle1.draw(12);
+        rectangle1.fill(20);
+        stickman1.draw(30);
+    }}
